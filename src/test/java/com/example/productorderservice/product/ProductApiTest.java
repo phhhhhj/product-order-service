ed1782bce6d3e0a3b76f885ba57e8b87a18b9f12
phhhhhj <lemon_0425@naver.com>
2023-05-26 10:16:34 +0900

    원복

--- src/test/java/com/example/productorderservice/product/ProductApiTest.java
+++ src/test/java/com/example/productorderservice/product/ProductApiTest.java
@@ -31,7 +31,6 @@ class ProductApiTest extends ApiTest {
 				.log().all().extract();
 
 		assertThat(response.statusCode()).isEqualTo(HttpStatus.CREATED.value());
-		System.out.println();
 	}
 
 	private AddProductRequest 상품등록요청_생성() {
